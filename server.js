cbac890e077e31bed6ef964aac86c0d43ba398e9
Andrey Shapovalov <llianandr94@gmai.com>
2022-02-08 12:06:15 +0200

    Example create image from dockerfile

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require ('express')();
+
+
+app.use((req,res,next)=>{
+  res.send('working my server');
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{console.log('server started at port ' + PORT)});
