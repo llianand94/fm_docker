450752d582cfa4d9fa71ea2852fd5ec712c02275
Andrey Shapovalov <llianandr94@gmai.com>
2022-02-09 09:26:15 +0200

    Add start.sh

--- server.js
+++ server.js
@@ -9,6 +9,3 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{console.log('server started at port ' + PORT)});
 
-
-// sudo docker build -t server:0.1 -f ./Dockerfile-dev .
-// sudo docker run -dit -p 8080:3000 -v $(pwd):/app:ro --name server_docker server:0.1
